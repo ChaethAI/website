cb4e1cce0319148668dedb4496f1a589eee61bd5
YOUR_CHAETHAI_USERNAME <YOUR_CHAETHAI_EMAIL>
2025-09-15 00:38:17 +0700

    small fixes on website

--- components/global/alternating_layout.tsx
+++ components/global/alternating_layout.tsx
@@ -13,10 +13,13 @@ export const AlternatingLayout = React.memo(function AlternatingLayout({
 }) {
   return (
     <div className={["grid grid-cols-1 md:grid-cols-2", className].join(" ")}> 
-      <div className={["order-1 bg-card p-0 md:p-6 md:aspect-square", leftGraphic ? "md:order-1" : "md:order-2"].join(" ")}> 
-        <div className="aspect-[4/3] md:aspect-auto size-full">{graphic}</div>
+      {/* Move padding inside to avoid Safari aspect-ratio quirk */}
+      <div className={["order-1 bg-card md:aspect-square", leftGraphic ? "md:order-1" : "md:order-2"].join(" ")}> 
+        <div className="size-full p-0 md:p-6">
+          <div className="aspect-[4/3] md:aspect-auto size-full">{graphic}</div>
+        </div>
       </div>
-      <div className={["order-2 bg-neutral-800 text-neutral-100 md:aspect-square", leftGraphic ? "md:order-2" : "md:order-1"].join(" ")}>
+      <div className={["order-2 bg-neutral-800 text-neutral-100 md:aspect-square", leftGraphic ? "md:order-2" : "md:order-1"].join(" ")}> 
         {content}
       </div>
     </div>
